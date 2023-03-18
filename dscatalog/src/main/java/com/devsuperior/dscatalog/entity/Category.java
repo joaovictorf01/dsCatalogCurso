2efcb1a76859143e18ef660ba6e5d0336e3c1edf
João Victor <joaovictorf01@gmail.com>
2023-03-18 00:35:34 -0300

    adicionando primeiro endpoint com consulta ao banco  de teste.

--- dscatalog/src/main/java/com/devsuperior/dscatalog/entity/Category.java
+++ dscatalog/src/main/java/com/devsuperior/dscatalog/entity/Category.java
@@ -1,12 +1,23 @@
 package com.devsuperior.dscatalog.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_category")
 @Setter
 @Getter
 public class Category {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 
