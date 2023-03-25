96de701cac946b0f2ba53408525f66b236650d16
João Victor <joaovictorf01@gmail.com>
2023-03-24 22:29:58 -0300

    Adicionando  Seeding da base de dados

---
+++ dscatalog/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_category (name) VALUES ('Instrumentos de corda');
+INSERT INTO tb_category (name) VALUES ('Instrumentos de sopro');
+INSERT INTO tb_category (name) VALUES ('Instrumentos de teclado');
+INSERT INTO tb_category (name) VALUES ('Instrumentos de percussão');
+INSERT INTO tb_category (name) VALUES ('Instrumentos eletrônicos');
